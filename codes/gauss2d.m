fe8138aaaf0d6d41f291f523d4692444f51af10b
Man Yi Yim <manyi.yim@gmail.com>
2021-03-17 11:44:23 -0500

    Add files via upload

--- codes/gauss2d.m
+++ codes/gauss2d.m
@@ -1,9 +1,9 @@
-function gau = gauss2d(Nx,Ny,X,Y,sig)
-if nargin == 2
-    X = ceil(Nx/2); Y = ceil(Ny/2);
-end
-if nargin <= 4
+function gau = gauss2d(Nx,Ny,sig,X,Y)
+if nargin <= 2
     sig = 5;
+end
+if nargin == 3
+    X = ceil(Nx/2); Y = ceil(Ny/2);
 end
 [x,y] = meshgrid(1:Nx,1:Ny);
 gau = exp(-(x-X).^2/(2*sig^2)-(y-Y).^2/(2*sig^2))/(2*pi*sig^2);
--- codes/imagesc_env.m
+++ codes/imagesc_env.m
@@ -1,3 +1,4 @@
 function imagesc_env(x,y,mat)
     imagesc(x,y,mat');
+    set(gca,'YDir','normal');
 end
