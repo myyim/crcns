5299f82bdd6b9179486fb3afb8c6a849052ce17a
Man Yi Yim <manyi.yim@gmail.com>
2021-04-05 09:21:32 -0500

    Add files via upload

--- codes/imagesc_env.m
+++ codes/imagesc_env.m
@@ -1,4 +1,10 @@
-function imagesc_env(x,y,mat)
-    imagesc(x,y,mat');
-    set(gca,'YDir','normal');
+function imagesc_env(mat,x,y)
+% imagesc_env(mat,x,y) returns the colormap for mat with binnings x and y
+% in the desirable format.
+if nargin == 1
+    x = 1:size(mat,1);
+    y = 1:size(mat,2);
+end
+imagesc(x,y,mat');
+set(gca,'YDir','normal');
 end
